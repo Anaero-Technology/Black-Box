1f4aee0ed833e843c7788f6a33c392ec0851d3ac
Jemhunter <goldmanrobbie@gmail.com>
2021-03-25 14:15:58 +0000

    Reinstated try except in case of failure

--- overallCalculations.py
+++ overallCalculations.py
@@ -67,128 +67,128 @@ def performGeneralCalculations(setupData, eventData) -> [list, str]:
         return [], "Setup file not formatted correctly, ensure that all fields are of the correct data types."
 
     #Attempt to process the event data
-    #try:
-    #Iterate through the events that occured
-    for event in eventData:
-        #Add relevant data for each event - update as each hour passes and each day (also store innoculum data and averages then adjust hourly values at threshold)
-        #Event - event id, time (s), bucket number, temperature (deg C), pressure (hPa)
+    try:
+        #Iterate through the events that occured
+        for event in eventData:
+            #Add relevant data for each event - update as each hour passes and each day (also store innoculum data and averages then adjust hourly values at threshold)
+            #Event - event id, time (s), bucket number, temperature (deg C), pressure (hPa)
 
-        #Get the time that the event occurred at
-        time = int(float(event[1]))
-        #if another hour has passed
-        if time - lastHour > hourLength:
+            #Get the time that the event occurred at
+            time = int(float(event[1]))
+            #if another hour has passed
+            if time - lastHour > hourLength:
 
-            #For each of the channels
-            for hourId in range(0, len(currentHour)):
+                #For each of the channels
+                for hourId in range(0, len(currentHour)):
 
-                #Add the current hour value to the list of hours
-                hourList[hourId].append(currentHour[hourId])
+                    #Add the current hour value to the list of hours
+                    hourList[hourId].append(currentHour[hourId])
 
-                #If this is one of the innculum only channels - add it's value (of volume per gram) to the list
-                if innoculumOnly[hourId]:
-                    hourInnoculum[-1].append(currentHour[hourId] / innoculumMass[hourId])
-
-                #Reset the current hour ready for the next one
-                currentHour[hourId] = 0
-
-                #Iterate through 1 to number of hours passed (in case multiple hours pass between tips)
-                for _ in range(1, int((time - lastHour) / hourLength)):
-                    #Add a 0 to the hour list
-                    hourList[hourId].append(0)
-                    #If this is innoculum - nothing passed here too
+                    #If this is one of the innculum only channels - add it's value (of volume per gram) to the list
                     if innoculumOnly[hourId]:
-                        hourInnoculum[-1].append(0)
+                        hourInnoculum[-1].append(currentHour[hourId] / innoculumMass[hourId])
+
+                    #Reset the current hour ready for the next one
+                    currentHour[hourId] = 0
+
+                    #Iterate through 1 to number of hours passed (in case multiple hours pass between tips)
+                    for _ in range(1, int((time - lastHour) / hourLength)):
+                        #Add a 0 to the hour list
+                        hourList[hourId].append(0)
+                        #If this is innoculum - nothing passed here too
+                        if innoculumOnly[hourId]:
+                            hourInnoculum[-1].append(0)
+                
+                #Add a new list to the hourly inocculum totals (for the new hour)
+                hourInnoculum.append([])
+                
+                #Add the necessary amount to the number of hours passed
+                for _ in range(0, int((time - lastHour) / hourLength)):
+                    lastHour = lastHour + hourLength
             
-            #Add a new list to the hourly inocculum totals (for the new hour)
-            hourInnoculum.append([])
-            
-            #Add the necessary amount to the number of hours passed
-            for _ in range(0, int((time - lastHour) / hourLength)):
-                lastHour = lastHour + hourLength
+            #Get the id of the channel
+            idNum = int(event[2]) - 1
+            #Calculate the volume - using gass constant, temperature and pressure
+            volume = gassConstants[idNum] * (float(event[3]) + 237.13) / float(event[4])
+            #Add this amount to the current hourly value for that channel
+            currentHour[idNum] = currentHour[idNum] + volume
         
-        #Get the id of the channel
-        idNum = int(event[2]) - 1
-        #Calculate the volume - using gass constant, temperature and pressure
-        volume = gassConstants[idNum] * (float(event[3]) + 237.13) / float(event[4])
-        #Add this amount to the current hourly value for that channel
-        currentHour[idNum] = currentHour[idNum] + volume
-    
-    #If the last innoculum value set is not filled - remove it
-    if len(hourInnoculum[-1]) == 0:
-        del hourInnoculum[-1]
+        #If the last innoculum value set is not filled - remove it
+        if len(hourInnoculum[-1]) == 0:
+            del hourInnoculum[-1]
 
-    #Iterate through the hours
-    for hourId in range(0, len(hourInnoculum)):
-        #Calculate the average gass volume produced of innoculum (per gram)
-        average = sum(hourInnoculum[hourId]) / len(hourInnoculum[hourId])
-        hourInnoculumAvg.append(average)
-    
-    #Iterate through the tubes
-    for tubeId in range(0, len(hourList)):
         #Iterate through the hours
-        for hourId in range(0, len(hourList[tubeId])):
-            #Calculate the total gass evolved that hour: volume - (average volume per g of innoculum * mass of innoculum)
-            extraGassEvolved = hourList[tubeId][hourId] - (hourInnoculumAvg[hourId] * innoculumMass[tubeId])
-            #Default volume of 0 per gram
-            extraGassEvolvedPerGram = 0
-            #If this tube contains a sample
-            if not innoculumOnly[tubeId] and sampleMass[tubeId] > 0:
-                #Divide by the mass in the tube to get volume per gram
-                extraGassEvolvedPerGram = extraGassEvolved / sampleMass[tubeId]
-            #Add volumes to lists total volume and per gram
-            if not innoculumOnly[tubeId]:
-                #If it is a sample tube
-                hourListWithoutInnoculum[tubeId].append(extraGassEvolved)
-                hourListWithoutInnoculumPerGram[tubeId].append(extraGassEvolvedPerGram)
-            else:
-                #If this is an innoculum tube
-                hourListWithoutInnoculum[tubeId].append(0)
-                hourListWithoutInnoculumPerGram[tubeId].append(0)
-    
-    #Iterate through the tubes
-    for tubeId in range(len(hourList)):
-        #Each of the totals is 0
-        hourCount = 0
-        dayVal = 0
-        dayValWithoutI = 0
-        dayValWithoutIPerGram = 0
-        dayValInnoculumAvg = 0
-        #Iterate through each hour
-        for hourNumber in range(0, len(hourList[tubeId])):
-            #Add the hour value to the running total for day values (volume, volume - innoculum, (volume - innoculum) / sample mass, innoculum average)
-            dayVal = dayVal + hourList[tubeId][hourNumber]
-            dayValWithoutI = dayValWithoutI + hourListWithoutInnoculum[tubeId][hourNumber]
-            dayValWithoutIPerGram = dayValWithoutIPerGram + hourListWithoutInnoculumPerGram[tubeId][hourNumber]
-            dayValInnoculumAvg = dayValInnoculumAvg + hourInnoculumAvg[hourNumber]
+        for hourId in range(0, len(hourInnoculum)):
+            #Calculate the average gass volume produced of innoculum (per gram)
+            average = sum(hourInnoculum[hourId]) / len(hourInnoculum[hourId])
+            hourInnoculumAvg.append(average)
+        
+        #Iterate through the tubes
+        for tubeId in range(0, len(hourList)):
+            #Iterate through the hours
+            for hourId in range(0, len(hourList[tubeId])):
+                #Calculate the total gass evolved that hour: volume - (average volume per g of innoculum * mass of innoculum)
+                extraGassEvolved = hourList[tubeId][hourId] - (hourInnoculumAvg[hourId] * innoculumMass[tubeId])
+                #Default volume of 0 per gram
+                extraGassEvolvedPerGram = 0
+                #If this tube contains a sample
+                if not innoculumOnly[tubeId] and sampleMass[tubeId] > 0:
+                    #Divide by the mass in the tube to get volume per gram
+                    extraGassEvolvedPerGram = extraGassEvolved / sampleMass[tubeId]
+                #Add volumes to lists total volume and per gram
+                if not innoculumOnly[tubeId]:
+                    #If it is a sample tube
+                    hourListWithoutInnoculum[tubeId].append(extraGassEvolved)
+                    hourListWithoutInnoculumPerGram[tubeId].append(extraGassEvolvedPerGram)
+                else:
+                    #If this is an innoculum tube
+                    hourListWithoutInnoculum[tubeId].append(0)
+                    hourListWithoutInnoculumPerGram[tubeId].append(0)
+        
+        #Iterate through the tubes
+        for tubeId in range(len(hourList)):
+            #Each of the totals is 0
+            hourCount = 0
+            dayVal = 0
+            dayValWithoutI = 0
+            dayValWithoutIPerGram = 0
+            dayValInnoculumAvg = 0
+            #Iterate through each hour
+            for hourNumber in range(0, len(hourList[tubeId])):
+                #Add the hour value to the running total for day values (volume, volume - innoculum, (volume - innoculum) / sample mass, innoculum average)
+                dayVal = dayVal + hourList[tubeId][hourNumber]
+                dayValWithoutI = dayValWithoutI + hourListWithoutInnoculum[tubeId][hourNumber]
+                dayValWithoutIPerGram = dayValWithoutIPerGram + hourListWithoutInnoculumPerGram[tubeId][hourNumber]
+                dayValInnoculumAvg = dayValInnoculumAvg + hourInnoculumAvg[hourNumber]
 
-            #Incrament hour
-            hourCount = hourCount + 1
-            #If a day has passed
-            if hourCount >= 24:
-                #Reset counter
-                hourCount = 0
-                #Add values to lists
+                #Incrament hour
+                hourCount = hourCount + 1
+                #If a day has passed
+                if hourCount >= 24:
+                    #Reset counter
+                    hourCount = 0
+                    #Add values to lists
+                    dayList[tubeId].append(dayVal)
+                    dayListWithoutInnoculum[tubeId].append(dayValWithoutI)
+                    dayListWithoutInnoculumPerGram[tubeId].append(dayValWithoutIPerGram)
+                    dayInnoculumAvg.append(dayValInnoculumAvg)
+                    #Reset values
+                    dayVal = 0
+                    dayValWithoutI = 0
+                    dayValWithoutIPerGram = 0
+                    dayValInnoculumAvg = 0
+            
+            #If hours have passed but not a full day (at the end)
+            if hourCount != 0:
+                #Add the remaining values to form a final (partial) day
                 dayList[tubeId].append(dayVal)
                 dayListWithoutInnoculum[tubeId].append(dayValWithoutI)
                 dayListWithoutInnoculumPerGram[tubeId].append(dayValWithoutIPerGram)
                 dayInnoculumAvg.append(dayValInnoculumAvg)
-                #Reset values
-                dayVal = 0
-                dayValWithoutI = 0
-                dayValWithoutIPerGram = 0
-                dayValInnoculumAvg = 0
-        
-        #If hours have passed but not a full day (at the end)
-        if hourCount != 0:
-            #Add the remaining values to form a final (partial) day
-            dayList[tubeId].append(dayVal)
-            dayListWithoutInnoculum[tubeId].append(dayValWithoutI)
-            dayListWithoutInnoculumPerGram[tubeId].append(dayValWithoutIPerGram)
-            dayInnoculumAvg.append(dayValInnoculumAvg)
-        
-    #except:
+            
+    except:
         #Something is wrong with the way the event log file is formatted - report error and stop
-        #return [], "Event file not formatted correctly, ensure that all fields are present and of the correct data type."
+        return [], "Event file not formatted correctly, ensure that all fields are present and of the correct data type."
 
     #Group setup information into list
     setup = [names, inUse, innoculumOnly, innoculumMass, sampleMass, tumblerVolume]
